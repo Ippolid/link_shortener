32fd55f0b6ff976577f1419f55342feb7d036d3c
Ippolid <iliamishin2006@gmail.com>
2024-12-02 00:24:38 +0300

    add handlers and server

---
+++ service/internal/server/Models.go
@@ -0,0 +1,22 @@
+package server
+
+import "time"
+
+type RequestCreateLink struct {
+	OldurL string `json:"oldurl" binding:"required"`
+	UserID string `json:"userid" binding:"required"`
+}
+
+// Структура для ответа
+type ResponseCreateLinkBody struct {
+	ShortURL string `json:"shorturl"`
+}
+
+type ResponseGetLinks struct {
+	Links []string `json:"links"`
+}
+
+type ResponseLinkStatistic struct {
+	ExpireTime    time.Time `json:"expiretime"`
+	TransferCount int       `json:"transferCount"`
+}
