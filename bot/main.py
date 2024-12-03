4258b920395aa6160812e5ca69a8ccae5cebd014
pollyncessa <pollyncessa@gmail.com>
2024-12-03 19:54:39 +0300

    add change

--- bot/main.py
+++ bot/main.py
@@ -14,7 +14,8 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-START_CHOICES, REPLY_FOR_CREATE, URL_CHOICES, REPLY_FOR_DELETE = range(4)
+START_CHOICES, REPLY_FOR_CREATE, URL_CHOICES, REPLY_FOR_DELETE, \
+REPLY_FOR_CHANGE, ASK_FOR_PERIOD, REPLY_FOR_CHANGE_PERIOD = range(7)
 
 create_url_btn = 'создать ссылку'
 to_main_page_btn = 'на главную'
@@ -67,50 +68,96 @@ async def create_url_get(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 
 
 async def list_of_urls(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    response = ['http', 'http']
-
+    response = ['http_1', 'http_2']
+    user_data = context.user_data
+    if not user_data.get('urls'):
+        user_data['urls'] = response
+    urls = user_data['urls']
+    urls_str = ''
+    for i in range(len(urls)):
+        urls_str += f"\n{i + 1} {urls[i]}"
     await update.message.reply_text(
-        f"список ссылок {response}",
+        f"список ссылок {urls_str}",
         reply_markup=urls_markups,
     )
     return URL_CHOICES
 
 
 async def delete_url_ask(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    text = update.message.text
-    context.user_data["choice"] = text
     await update.message.reply_text(f"введите номер ссылки, которую удалить")
 
     return REPLY_FOR_DELETE
 
 
 async def delete_url_get(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    response = 'okay'
+    text = update.message.text
+    user_data = context.user_data
+    user_data['urls'].pop(int(text) - 1)
+    print(user_data['urls'])
+    response = '200_OK'
 
     await update.message.reply_text(
-        f"ваша ссылка {response}",
-        reply_markup=start_markups,
+        f"удаление ссылки: {response}",
+        reply_markup=urls_markups,
     )
 
-    return START_CHOICES
+    return URL_CHOICES
+
+
+async def change_url_ask(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    text = update.message.text
+    context.user_data["choice"] = text
+    await update.message.reply_text(f"введите номер ссылки, срок действия которой хотите изменить")
+
+    return REPLY_FOR_CHANGE
+
+
+async def change_url_get(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    response = ['http', '30 дней']
+    await update.message.reply_text(
+        f"ваша ссылка {response[0]}, срок действия {response[1]}"
+    )
+    await update.message.reply_text(
+        'Введите новый срок действия в днях',
+        reply_markup=urls_markups,
+    )
+
+    return REPLY_FOR_CHANGE_PERIOD
+
+#
+# async def change_url_period_ask(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+#     # text = update.message.text
+#     # context.user_data["choice"] = text
+#     await update.message.reply_text(f"введите желаемый срок действия")
+#
+#     return REPLY_FOR_CHANGE_PERIOD
+
+
+async def change_url_period_get(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    response = '200_OK'
+    new_period = update.message.text
+    await update.message.reply_text(
+        f'ответ: {response}, новый срок действия: {new_period}',
+        reply_markup=urls_markups,
+    )
+
+    return URL_CHOICES
 
 
 async def to_main_page(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text(
         'start',
         reply_markup=start_markups,
-        )
+    )
     return START_CHOICES
 
-TOKEN = '7927553615:AAGSdUqicPUs3GIRRtjO3j7Fe29KDsdVdK0'
+
+TOKEN = ''
 
 
 def main() -> None:
-    """Run the bot."""
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token(TOKEN).build()
 
-    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
@@ -132,9 +179,9 @@ def main() -> None:
                 MessageHandler(
                     filters.Regex(f"^({delete_url_btn})$"), delete_url_ask
                 ),
-                # MessageHandler(
-                #     filters.Regex(f"^({change_url_btn})$"), change_url
-                # ),
+                MessageHandler(
+                    filters.Regex(f"^({change_url_btn})$"), change_url_ask
+                ),
                 MessageHandler(
                     filters.Regex(f"^({to_main_page_btn})$"), to_main_page
                 ),
@@ -143,6 +190,24 @@ def main() -> None:
                 MessageHandler(
                     filters.TEXT & ~(filters.COMMAND | filters.Regex(f"^({to_main_page_btn})$")),
                     delete_url_get,
+                ),
+            ],
+            REPLY_FOR_CHANGE: [
+                MessageHandler(
+                    filters.TEXT & ~(filters.COMMAND | filters.Regex(f"^({to_main_page_btn})$")),
+                    change_url_get,
+                ),
+            ],
+            # ASK_FOR_PERIOD: [
+            #     MessageHandler(
+            #         filters.TEXT & ~(filters.COMMAND | filters.Regex(f"^({to_main_page_btn})$")),
+            #         change_url_period_get,
+            #     ),
+            # ],
+            REPLY_FOR_CHANGE_PERIOD: [
+                MessageHandler(
+                    filters.TEXT & ~(filters.COMMAND | filters.Regex(f"^({to_main_page_btn})$")),
+                    change_url_period_get,
                 ),
             ],
         },
